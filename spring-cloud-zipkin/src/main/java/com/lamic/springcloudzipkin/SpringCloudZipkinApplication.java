3b5df9d17baf822579c480d763aee61250fc09cf
772010988@qq.com <772010988@qq.com>
2018-06-11 17:52:17 +0800

    sleuth

---
+++ spring-cloud-zipkin/src/main/java/com/lamic/springcloudzipkin/SpringCloudZipkinApplication.java
@@ -0,0 +1,17 @@
+package com.lamic.springcloudzipkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+import zipkin.server.EnableZipkinServer;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableZipkinServer
+public class SpringCloudZipkinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudZipkinApplication.class, args);
+	}
+}
