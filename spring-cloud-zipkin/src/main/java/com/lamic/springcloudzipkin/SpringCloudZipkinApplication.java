5d41d858140b3821159dc1a8d19ffbc30f66d29f
772010988@qq.com <772010988@qq.com>
2018-06-12 14:54:05 +0800

    mysql

--- spring-cloud-zipkin/src/main/java/com/lamic/springcloudzipkin/SpringCloudZipkinApplication.java
+++ spring-cloud-zipkin/src/main/java/com/lamic/springcloudzipkin/SpringCloudZipkinApplication.java
@@ -1,10 +1,14 @@
 package com.lamic.springcloudzipkin;
 
+import javax.sql.DataSource;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 
 import zipkin.server.EnableZipkinServer;
+import zipkin.storage.mysql.MySQLStorage;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -14,5 +18,10 @@ public class SpringCloudZipkinApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCloudZipkinApplication.class, args);
+	}
+	
+	@Bean
+	public MySQLStorage mySQLStorage(DataSource datasource) {
+		return MySQLStorage.builder().datasource(datasource).executor(Runnable::run).build();
 	}
 }
